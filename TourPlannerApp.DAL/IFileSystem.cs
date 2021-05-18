d0a4f7fcacb9bd97b1748430c393b1bbd3f0f295
pixelini <eglatz@gmx.at>
2021-05-18 20:02:16 +0200

    file system handle

---
+++ TourPlannerApp.DAL/IFileSystem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TourPlannerApp.Models;
+
+namespace TourPlannerApp.DAL
+{
+    public interface IFileSystem
+    {
+        public byte[] GetImg(TourItem tourItem);
+        public string SaveImg(TourItem tourItem, string fileName);
+    }
+}
